d34e28f1822083bee96fa08a35aa10f64212dc40
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2025-10-13 18:36:31 +0200

    Event Model

---
+++ Models/Event.cs
@@ -0,0 +1,15 @@
+﻿namespace Prog7312_MunicipalityApp_ST10299399.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public string? ImageUrl { get; set; }
+
+        public DateTime PostedDate { get; set; } = DateTime.Now;
+    }
+}
