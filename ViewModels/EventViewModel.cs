091921fbf4797bbe9b29db4f59396837d862d069
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2025-10-13 21:16:58 +0200

    Added EventViewModel

---
+++ ViewModels/EventViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prog7312_MunicipalityApp_ST10299399.Models;
+
+namespace Prog7312_MunicipalityApp_ST10299399.ViewModels
+{
+    public class EventViewModel
+    {
+        public SortedDictionary<DateTime, List<Event>> EventsByDate { get; set; }
+        public IEnumerable<Event> SearchResults { get; set; }
+        public HashSet<string> AllCategories { get; set; }
+        public string SelectedCategory { get; set; }
+        public string SearchQuery { get; set; }
+        public Queue<Event> QuickAnnouncements { get; set; }
+
+    }
+}
