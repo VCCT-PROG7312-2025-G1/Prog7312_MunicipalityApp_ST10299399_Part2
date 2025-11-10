f72ffc07ec23fc30399ffc7c3e8eecc484a02ac0
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2025-11-10 18:56:45 +0200

    Added the flow of how the status should be updated

---
+++ DataStructures/StatusGraph.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Prog7312_MunicipalityApp_ST10299399.DataStructures
+{
+    public class StatusGraph
+    {
+        private Dictionary<string, List<string>> _adjacencyList;
+        public StatusGraph()
+        {
+            _adjacencyList = new Dictionary<string, List<string>>
+            {
+                { "Reported", new List<string> { "InProgress", "Closed" } },
+                { "InProgress", new List<string> { "Resolved", "Closed" } },
+                { "Resolved", new List<string> { "Closed" } },
+                { "Closed", new List<string> { }},
+            };
+        }
+
+        public bool IsValidTransition(string currentStatus, string newStatus)
+        {
+            if (!_adjacencyList.ContainsKey(currentStatus))
+            {
+                 return false;
+            }
+
+            var possibleTransitions = _adjacencyList[currentStatus];
+            return possibleTransitions.Contains(newStatus);
+        }
+    }
+}
