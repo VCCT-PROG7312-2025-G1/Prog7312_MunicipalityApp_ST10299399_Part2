4e33e6b5ceaeaf8e8c0df19954e5f2e41365367d
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2025-11-10 16:35:56 +0200

    added helper for the max heap to move high priority issues to the top

---
+++ Helpers/IssuePriority.cs
@@ -0,0 +1,25 @@
+﻿namespace Prog7312_MunicipalityApp_ST10299399.Helpers
+{
+    public static class IssuePriority
+    {
+        private const int High_Priority = 100;
+        private const int Medium_Priority = 50;
+        private const int Low_Priority = 10;
+
+        public static int GetPriorityValue(string issueType)
+        {
+            return issueType.ToLower() switch
+            {
+                "Load shedding" => High_Priority,
+                "water outage" => High_Priority,
+                "pothole" => High_Priority,
+                "streetlight outage" => Medium_Priority,
+                "graffiti" => Low_Priority,
+                _ => Low_Priority,
+            };
+        }
+
+
+
+    }
+}
