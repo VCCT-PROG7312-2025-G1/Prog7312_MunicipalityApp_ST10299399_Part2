4cf13ce21e7ea9650f80ba098ccf915396a7ea1a
Ethan Scott Parsons <ST10299399@vcconnect.edu.za>
2025-11-10 10:45:54 +0200

    Added binary search tree model

---
+++ Models/BinarySearchTreeNode.cs
@@ -0,0 +1,18 @@
+﻿using Prog7312_MunicipalityApp_ST10299399.Models;
+
+namespace Prog7312_MunicipalityApp_ST10299399.Models
+{
+    public class BinarySearchTreeNode
+    {
+        public Issue Data { get; set; }
+        public BinarySearchTreeNode Left { get; set; }
+        public BinarySearchTreeNode Right { get; set; }
+        public BinarySearchTreeNode(Issue issue)
+        {
+            Data = issue;
+            Left = null;
+            Right = null;
+        }
+
+    }
+}
